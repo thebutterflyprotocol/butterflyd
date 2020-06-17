a7dbd74c4f86fc7085965f520ecf8dbadbe1494f
Tristan B. Kildaire <deavmi@disroot.org>
2020-06-17 13:40:52 +0200

    Added new type `Mail`

--- source/client/mail.d
+++ source/client/mail.d
@@ -1,6 +1,22 @@
 module client.mail;
 
+/**
+* Mailbox
+*
+* This represents an in-memory representation of a user's
+* mailbox.
+*/
 public final class Mailbox
+{
+
+}
+
+/**
+* Mail
+*
+* This represents an in-memory representation of a mail message.
+*/
+public final class Mail
 {
     
 }
