729583e3b1dabd099a3f18959237a36a816672bc
Tristan B. Kildaire <deavmi@disroot.org>
2020-06-17 11:32:35 +0200

    Start temporary server for testing i app.d

--- source/app.d
+++ source/app.d
@@ -1,6 +1,15 @@
 import std.stdio;
 
+import server.server : ButterflyServer;
+import std.socket : Address, parseAddress;
+
 void main()
 {
 	writeln("Edit source/app.d to start your project.");
+
+	Address address;
+
+	address = parseAddress("0.0.0.0:2223");
+
+	ButterflyServer server = new ButterflyServer(address);
 }
