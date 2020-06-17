0206aff0a70936e9962f3ddbaa03a578a186442d
Tristan B. Kildaire <deavmi@disroot.org>
2020-06-17 13:41:57 +0200

    Added new type `Folder`

--- source/client/mail.d
+++ source/client/mail.d
@@ -12,11 +12,22 @@ public final class Mailbox
 }
 
 /**
+* Folder
+*
+* This represents an in-memory representation of a folder
+* that resides in another folder or mailbox.
+*/
+public final class Folder
+{
+    
+}
+
+/**
 * Mail
 *
 * This represents an in-memory representation of a mail message.
 */
 public final class Mail
 {
-    
+
 }
