ed8ac7f71c562d4f30907195b6e00d1572cc440e
Tristan B. Kildaire <deavmi@disroot.org>
2020-06-17 13:26:30 +0200

    Added receive to read-write loop.

--- source/client/client.d
+++ source/client/client.d
@@ -2,8 +2,9 @@ module client.client;
 
 import core.thread : Thread;
 import std.socket : Socket;
-
 import bmessage;
+import std.stdio;
+import std.json;
 
 public final class ButterflyClient : Thread
 {
@@ -25,10 +26,36 @@ public final class ButterflyClient : Thread
 
     private void run()
     {
+        /* The received command block */
+        JSONValue commandBlock;
+
+        /* The received bytes */
+        byte[] receivedBytes;
+
         /* TODO: Implement loop read-write here */
         while(active)
         {
+            writeln("Awaiting command from client...");
 
+            /* Await a message from the client */
+            bool recvStatus = receiveMessage(clientSocket, receivedBytes);
+            
+            if(recvStatus)
+            {
+                /* TODO: Add error handling catch for all JSON here */
+
+                /* Parse the incoming JSON */
+                commandBlock = parseJSON(cast(string)receivedBytes);
+
+                /* Get the command */
+                string command = commandBlock["command"].str();
+
+                /* TODO: Add command handling here */
+            }
+            else
+            {
+                /* TODO: Add error handling here */
+            }
         }
 
         /* Close the socket */
