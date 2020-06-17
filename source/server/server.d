8936fc6854aeb30d56b5eebf75a29d4e81ae432e
Tristan B. Kildaire <deavmi@disroot.org>
2020-06-17 11:09:08 +0200

    Added basic block for looping socket connection dequeuing.

--- source/server/server.d
+++ source/server/server.d
@@ -1,6 +1,36 @@
 module server.server;
 
-public final class ButterflyServer
+import core.thread : Thread;
+import std.socket : Socket;
+
+public final class ButterflyServer : Thread
 {
-    
+
+    /**
+    * Socket to listen for incoming connections on
+    */
+    private Socket serverSocket;
+
+    /**
+    * Whether or not the server is active
+    */
+    private bool active;
+
+    this()
+    {
+        super(&run);
+    }
+
+    private void run()
+    {
+        /* TODO: Loop here */
+        while(active)
+        {
+            /* Block for an incoming queued connection */
+            Socket clientSocket = serverSocket.accept();
+
+            /* TODO: Add to array, create client and start client */
+        }
+
+    }
 }
