a0a1177d2bfde74043e718d48de6bedf7aaa9b1f
Tristan B. Kildaire <deavmi@disroot.org>
2020-06-18 17:48:49 +0200

    Changed listen port.

--- source/app.d
+++ source/app.d
@@ -8,6 +8,6 @@ void main()
 	writeln("Starting butterflyd...");
 
 	/* Start the server */
-	Address address = parseAddress("0.0.0.0", 2223);
+	Address address = parseAddress("0.0.0.0", 6969);
 	ButterflyServer server = new ButterflyServer(address);
 }
