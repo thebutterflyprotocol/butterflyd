1e66747307243e73d5821b4676c0d8299e991cf3
Tristan B. Kildaire <deavmi@disroot.org>
2020-06-19 12:53:06 +0200

    Daemon now reads configuration parameters from a config file.

--- source/app.d
+++ source/app.d
@@ -2,12 +2,25 @@ import std.stdio;
 
 import server.server : ButterflyServer;
 import std.socket : Address, parseAddress;
+import std.file;
+import std.json;
 
 void main()
 {
 	writeln("Starting butterflyd...");
 
+	JSONValue config;
+
+	File configFile;
+	configFile.open("butterflyd.json", "rb");
+	byte[] bytes;
+	bytes.length = configFile.size();
+	bytes = configFile.rawRead(bytes);
+	configFile.close();
+
+	config = parseJSON(cast(string)bytes);
+
 	/* Start the server */
-	Address address = parseAddress("0.0.0.0", 6969);
-	ButterflyServer server = new ButterflyServer(address, "poes");
+	Address address = parseAddress(config["address"].str(), 6969);
+	ButterflyServer server = new ButterflyServer(address, config["domain"].str());
 }
